6a488c17fc70ebee118ddba716412e454abc23c8
totto357 <totto357@gmail.com>
2019-01-09 23:41:11 +0900

    feat(#8) deep_freezeを利用するクラスを追加

---
+++ lib/bank.rb
@@ -0,0 +1,11 @@
+require "./lib/deep_freezable"
+
+class Bank
+  extend DeepFreezable
+
+  CURRENCIES = deep_freeze({
+    "Japan" => "yen",
+    "US" => "dollar",
+    "India" => "rupee",
+  })
+end
